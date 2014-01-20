a3d336b4417e334a5e3f702f8ab50b42bc66a6e5
Jonas Acres <acresjonas@gmail.com>
2014-01-20 14:27:17 -0800

    Added GET /people handler; logic to populate last_seen_epoch field

--- jedisense.rb
+++ jedisense.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/ruby
 
+require 'date'
 require 'mongo'
 require 'rubygems'
 require 'sinatra'
@@ -13,7 +14,44 @@ MONGO_HOST = ENV['MONGO_HOST']
 MONGO_PORT = ENV['MONGO_PORT']
 MONGO_DBNAME = ENV['MONGO_DBNAME']
 
+def epochForTimeSeen(ts)
+  # eg "Mon Jan 20 20:10:17.418 UTC 2014", [ Weekday, Month, Day, Timecode, UTC, Year ]
+  comps = ts.split(" ")
+  months = { "Jan"=>"01", "Feb"=>"02", "Mar"=>"03", "Apr"=>"04", "May"=>"05", "Jun"=>"06", "Jul"=>"07", "Aug"=>"08", "Sep"=>"09", "Oct"=>"10", "Nov"=>"11", "Dec"=>"12" }
+  month = months[comps[1]]
+  timestr = comps[3].split(".")[0]
+  formatted = "#{comps[5]}-#{month}-#{comps[2]} #{timestr}"
+  date = DateTime.strptime(formatted, "%Y-%m-%d %H:%M:%S")
+  
+  return date.strftime("%s").to_i
+end
+
 db = MongoClient.new(MONGO_HOST, MONGO_PORT).db(MONGO_DBNAME)
+
+get '/people' do
+  content_type :json
+  
+  devices = db["devices"].find().to_a
+  usersByMac = {}
+  devices.each do |device|
+    usersByMac[device["name"]]
+  end
+  
+  minTimestamp = Time.new().to_i - 60*10
+  clients = db["clients"].find({
+      "$query" => { "last_seen_epoch" => { "$gt" => minTimestamp } },
+    "$orderby" => { "last_seen_epoch" => -1 }
+    })
+  
+  seenRecords = {}
+  clients.each do |client|
+    next if seenRecords.has_key?(client["mac"])
+    client["user"] = usersByMac[client["mac"]]
+    seenRecords[client["mac"]] = client
+  end
+  
+  seenRecords.to_json
+end
 
 get '/events' do
   VALIDATOR
@@ -27,12 +65,9 @@ post '/events' do
   end
   
   map['probing'].each do |c|
-    cleanedClient = c
-    components = c['client_mac'].split(" ")
-    cleanedClient['client_mac'] = components[0]
-    cleanedClient['timestamp'] = components[1]
-    db["clients"].insert(cleanedClient);
-    logger.info "client #{cleanedClient['client_mac']} seen on ap #{c['ap_mac']} with rssi #{c['rssi']} at #{c['last_seen']}"
+    c["last_seen_epoch"] = epochForTimeSeen(c["last_seen"])
+    db["clients"].insert(c);
+    logger.info "client #{c['client_mac']} seen on ap #{c['ap_mac']} with rssi #{c['rssi']} at #{c['last_seen']}"
   end
   ""
 end
