3a2cff2119e48c2f120d94c83f0425290f5e2561
Jonas Acres <acresjonas@gmail.com>
2014-01-23 15:30:09 -0800

    Modified Jedisense to use HTTPS and require environment-configured bearer token to access /people.

--- jedisense.rb
+++ jedisense.rb
@@ -7,6 +7,9 @@ require 'sinatra'
 require 'json'
 require 'net/http'
 require 'rest-client'
+require 'webrick'
+require 'webrick/https'
+require 'openssl'
 
 include Mongo
 
@@ -40,54 +43,87 @@ def sendEmail(subject, body, recipients)
     :html => body
 end
 
-get '/people' do
-  content_type :json
-  
-  devices = $db["devices"].find().to_a
-  usersByMac = {}
-  devices.each do |device|
-    usersByMac[device["mac"]] = device["name"]
-  end
-  
-  minTimestamp = Time.new().to_i - 60*10
-  clients = $db["clients"].find({
-      "$query" => { "last_seen_epoch" => { "$gt" => minTimestamp } },
-    "$orderby" => { "last_seen_epoch" => -1 }
-    })
-  
-  seenRecords = {}
-  clients.each do |client|
-    next if seenRecords.has_key?(client["client_mac"])
-    client["user"] = usersByMac[client["client_mac"]]
-    recordKeys = ["client_mac", "ap_mac", "last_seen_epoch", "rssi", "user", "site"]
-    record = {}
-    recordKeys.each do |key|
-      record[key] = client[key] if client[key] != nil
+class JediSense < Sinatra::Base
+  get '/people' do
+    if request.env["HTTP_AUTHORIZATION"]
+      comps = request.env["HTTP_AUTHORIZATION"].split(' ')
+      if(comps.length !=2 || comps[0] != "Bearer" || comps[1] != ENV["ACCESS_TOKEN"]) then
+        status 403
+        body "Invalid token\n"
+        return
+      end
+    else
+      puts "No authentication\n"
+      status 401
+      body "Authentication required\n"
+      return
     end
     
-    seenRecords[client["client_mac"]] = record
+    content_type :json
+
+    devices = $db["devices"].find().to_a
+    usersByMac = {}
+    devices.each do |device|
+      usersByMac[device["mac"]] = device["name"]
+    end
+
+    minTimestamp = Time.new().to_i - 60*10
+    clients = $db["clients"].find({
+        "$query" => { "last_seen_epoch" => { "$gt" => minTimestamp } },
+      "$orderby" => { "last_seen_epoch" => -1 }
+      })
+
+    seenRecords = {}
+    clients.each do |client|
+      next if seenRecords.has_key?(client["client_mac"])
+      client["user"] = usersByMac[client["client_mac"]]
+      recordKeys = ["client_mac", "ap_mac", "last_seen_epoch", "rssi", "user", "site"]
+      record = {}
+      recordKeys.each do |key|
+        record[key] = client[key] if client[key] != nil
+      end
+
+      seenRecords[client["client_mac"]] = record
+    end
+
+    seenRecords.values.to_json
   end
-  
-  seenRecords.values.to_json
+
+  get '/events/:site' do
+    $VALIDATOR
+  end
+
+  post '/events/:site' do
+    map = JSON.parse(params[:data])
+    if map['secret'] != $SECRET
+      logger.warn "got post with bad secret: #{map['secret']}"
+      return
+    end
+
+    map['probing'].each do |c|
+      c["last_seen_epoch"] = epochForTimeSeen(c["last_seen"])
+      c["site"] = params["site"]
+
+      $db["clients"].insert(c);
+      logger.info "client #{c['client_mac']} seen on ap #{c['ap_mac']} with rssi #{c['rssi']} at #{c['last_seen']}"
+    end
+    ""
+  end
 end
 
-get '/events/:site' do
-  $VALIDATOR
+app = Rack::Builder.new do
+  map '/' do
+    run JediSense
+  end
 end
 
-post '/events/:site' do
-  map = JSON.parse(params[:data])
-  if map['secret'] != $SECRET
-    logger.warn "got post with bad secret: #{map['secret']}"
-    return
-  end
-  
-  map['probing'].each do |c|
-    c["last_seen_epoch"] = epochForTimeSeen(c["last_seen"])
-    c["site"] = params["site"]
-    
-    $db["clients"].insert(c);
-    logger.info "client #{c['client_mac']} seen on ap #{c['ap_mac']} with rssi #{c['rssi']} at #{c['last_seen']}"
-  end
-  ""
-end
+cert = OpenSSL::X509::Certificate.new File.read '_ssl/certificate.crt'
+pkey = OpenSSL::PKey::RSA.new File.read '_ssl/private.key'
+webrickOpts = {
+  :Port => 8000,
+  :SSLEnable => true,
+  :SSLCertificate => cert,
+  :SSLPrivateKey => pkey
+}
+
+Rack::Handler::WEBrick.run app, webrickOpts
