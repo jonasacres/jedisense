4aa323afc014d2565584b62d6b56221ce0a9a010
Jonas Acres <acresjonas@gmail.com>
2014-01-23 16:00:42 -0800

    Revised whosin to pass authentication

--- whosin.rb
+++ whosin.rb
@@ -1,13 +1,18 @@
 #!/usr/bin/ruby
 
 require 'json'
-require 'net/http'
+require 'net/https'
 
-url = URI.parse('http://presence.acreswonder.com:3200/people')
-req = Net::HTTP::Get.new(url.path)
-res = Net::HTTP.start(url.host, url.port) {|http|
-  http.request(req)
-}
+uri = URI.parse('https://presence.a4sw.co:3310/people')
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+
+req = Net::HTTP::Get.new(uri.request_uri)
+if ARGV.length >= 1 then
+  req.add_field("Authorization", "Bearer #{ARGV[0]}")
+end
+
+res = http.request(req)
 
 clients = JSON.parse(res.body)
 clientsByOffice = {}
